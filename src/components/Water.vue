4f87a21620bb853b4d90a801d81c4c66c0bb300e
h3xar0n <aron@eideldev.com>
2019-01-16 20:34:58 -0800

    update information and logo to kong

--- src/components/Water.vue
+++ src/components/Water.vue
@@ -1,37 +1,23 @@
 <template>
-	<section class="water news">
-      <div>
-        <h2>Join us at
-          <a href="https://goo.gl/maps/5CzGkbvc3Bs" target="_blank">
-            <img id="okta-logo" src="https://i.imgur.com/KLhqUhg.png" alt="">
-          </a>
-        </h2>
-        <br>
-        <a target="_blank" href="https://www.meetup.com/Point-of-Vue/">
-          <h3>
-            6PM-9PM August 1, 2018
-          </h3>
-        </a>
-      </div>
-      <br class="standard-break">
-      <div class="container">
-        <div class='row'>
-          <Mailchimp-Form></Mailchimp-Form>
-        </div>
-      </div>
-    </section>
+  <section class="water news">
+    <div>
+      <h2>Join us at</h2>
+      <a href="https://konghq.com/" target="_blank">
+        <img id="kong-logo" src="../assets/kong.svg" alt>
+      </a>
+      <a target="_blank" href="https://www.meetup.com/vue-sf/events/257831667/">
+        <h3>6PM-9PM January 24, 2019</h3>
+      </a>
+    </div>
+  </section>
 </template>
-<script>
-	export default {
 
-	}
-</script>
 <style lang="scss">
 .water {
   flex: auto;
   padding: 3vw;
   color: #ffffff;
-  background: linear-gradient(to bottom, #a9ccff,#4780d4);
+  background: linear-gradient(to top, #9ddbff, #4780d4);
 }
 
 .water * {
@@ -40,20 +26,15 @@
 
 .water h2 {
   font-size: 5vw;
+  padding: 0;
+  margin: 0 0 1vw 0;
 }
 
 .water h3 {
   font-size: 3.5vw;
 }
 
-#okta-logo {
-  width: 12vw;
+#kong-logo {
+  width: 24vw;
 }
-
-.standard-break {
-  line-height: 6vw;
-}
-
-
-
 </style>
